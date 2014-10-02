c33bdc8ebed4d654f5988728b4340c48dd519d50
Jiayu Zhou <dearjiayu@gmail.com>
2014-10-01 22:58:53 -0700

    FLAG_MAXFUNEV bug
    Fixed a naming issue

--- private/pnopt_flags.m
+++ private/pnopt_flags.m
@@ -1,6 +1,6 @@
 % pnopt_flags 
 % 
-%   $Revision: 0.8.0 $  $Date: 2012/12/01 $
+%   $Revision: 0.8.0 $  $Date: 2014/10/01 $
 % 
 
   SWITCH_OPTIM   = true;
@@ -10,12 +10,13 @@
   SWITCH_MAXFEV  = true;
   SWITCH_OTHER   = true;
 
-  FLAG_OPTIM   = 1;
-  FLAG_XTOL    = 2;
-  FLAG_FTOL    = 3;
-  FLAG_MAXITER = 4;
-  FLAG_MAXFEV  = 5;
-  FLAG_OTHER   = 6;
+  FLAG_OPTIM    = 1;
+  FLAG_XTOL     = 2;
+  FLAG_FTOL     = 3;
+  FLAG_MAXITER  = 4;
+  FLAG_MAXFUNEV = 5;
+  FLAG_MAXFEV   = 5;
+  FLAG_OTHER    = 6;
   
   MESSAGE_OPTIM   = 'Optimality below optim_tol.';
   MESSAGE_XTOL    = 'Relative change in x below xtol.';
